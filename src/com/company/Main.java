dfc3514cf61fe90a9992a8955b0ddc334a00cfec
al5091 <angelovinolim@gmail.com>
2018-11-28 11:21:56 -0500

    ahhh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,32 @@
 package com.company;
-import java.util.Arrays;
+
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        int array[] = new int[10000];
+        ArrayList<Number> array = new ArrayList<>();
         int counter = 0;
         int iterations = 100;
 
 	    for (int n = 0; n < iterations; n++)
         {
-            int num = (int)((Math.random() + 0.01) * 100);
-            array[n] = num;
-            counter++;
+            ArrayList<Number> nums = new ArrayList<>();
+            for (int j = 1; j <= 100; j++)
+                nums.add(new Integer(j));
+            counter = 0;
+            while (nums.size() != 0)
+            {
+                counter++;
+                int num = (int) ((Math.random() + 0.01) * 100);
+                for (int m = 0; m < nums.size(); m++)
+                {
+                    if (nums[m] == num)
+                        nums.remove(m);
+                }
+
+            }
         }
 
         for (int i = 0; i < 100; i ++)
