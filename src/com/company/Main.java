c2c668fbfc92f12b1db7e79dd0ed6117103b6fdd
Angelo Lim <angelovinolim@gmail.com>
2018-11-29 19:42:12 -0500

    ack

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,94 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int array[] = new int[100];
-        int counter = 0;
+        List iterationCount = new ArrayList<>();
+        int counter;
         int iterations = 100;
-        System.out.println(array.length);
+        double average = 0;
+        int array[] = new int[100000];
 
-	    for (int n = 0; n < iterations; n++)
-        {
-            for (int j = 0; j < 100; j++)
-            {
-                array[j] = j + 1;
-            }
-            counter = 0;
-            while (array.length != 0)
-            {
-                counter++;
-                int num = (int) ((Math.random() + 0.01) * 100);
-                for (int m = 0; m < array.length; m++)
-                {
-                    if (array[m] == num)
-                    {
-                        array[m] = 0;
+        while (iterations <= 10000) {
+            for (int n = 0; n < iterations; n++) {
+                List<Integer> numsArray = new ArrayList<>();
+
+                for (int j = 1; j <= 100; j++) {
+                    numsArray.add(j);
+                }
+                counter = 0;
+
+                while (numsArray.size() != 0) {
+                    counter++;
+                    int num = (int) ((Math.random() + 0.01) * 100);
+
+                    for (int m = 0; m < numsArray.size(); m++) {
+                        if (numsArray.get(m) == num) {
+                            numsArray.remove(m);
+                        }
                     }
                 }
 
+                iterationCount.add(counter);
             }
+
+            for (int i = 0; i < iterationCount.size(); i++) {
+
+                array[i] = (int) iterationCount.get(i);
+                average += array[i];
+            }
+            average /= iterationCount.size();
+            System.out.println(iterations + " Runs:   " + average);
+            iterations *= 10;
         }
-
-        for (int i = 0; i < 100; i ++)
-        {
-            System.out.println(array[i]);
-        }
-
-        System.out.println("Program ran " + counter + " times.");
-
     }
 }
+/*package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Average for 100 Runs : " + Run(100));
+        System.out.println("Average for 1000 Runs : " + Run(1000));
+        System.out.println("Average for 10000 Runs : " + Run(10000));
+    }
+    private static double Run(int iteration)
+    {
+        List<Integer> counts = new ArrayList<>();
+        for(int i=0; i<iteration;i++)
+        {
+            List<Integer> checklist = new ArrayList<>();
+            for(int j = 1;j<=100;j++)
+                checklist.add(j);
+            int counter = 0;
+            while(checklist.size() != 0)
+            {
+                counter++;
+                int random = ((int)(100 * Math.random()))+1;
+                for(int j = 0; j<checklist.size();j++)
+                {
+                    if (checklist.get(j) == random)
+                        checklist.remove(j);
+                }
+            }
+            counts.add(counter);
+        }
+        double average = 0;
+        System.out.println(counts.size());
+        System.out.println(counts.get(1));
+        for(int i = 0; i <counts.size();i++)
+        {
+            average += counts.get(i);
+        }
+        //average /= counts.size();
+        return average;
+    }
+}*/
